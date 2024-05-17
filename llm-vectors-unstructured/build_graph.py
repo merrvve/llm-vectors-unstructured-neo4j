be8aefec0e9c8a0d7e0d7b053eb53c1a5a382862
mervs <mervsen@gmail.com>
2024-05-17 23:23:12 +0300

    create graph from unstructured data

--- llm-vectors-unstructured/build_graph.py
+++ llm-vectors-unstructured/build_graph.py
@@ -20,16 +20,96 @@ text_splitter = CharacterTextSplitter(
 
 chunks = text_splitter.split_documents(docs)
 
-# Create a function to get the embedding
+# tag::get_embedding[]
+def get_embedding(llm, text):
+    response = llm.embeddings.create(
+            input=chunk.page_content,
+            model="text-embedding-ada-002"
+        )
+    return response.data[0].embedding
+# end::get_embedding[]
 
-# Create a function to get the course data
+# tag::get_course_data[]
+def get_course_data(llm, chunk):
+    data = {}
 
-# Create OpenAI object
+    path = chunk.metadata['source'].split(os.path.sep)
 
-# Connect to Neo4j
+    data['course'] = path[-6]
+    data['module'] = path[-4]
+    data['lesson'] = path[-2]
+    data['url'] = f"https://graphacademy.neo4j.com/courses/{data['course']}/{data['module']}/{data['lesson']}"
+    data['text'] = chunk.page_content
+    data['embedding'] = get_embedding(llm, data['text'])
 
-# Create a function to run the Cypher query
+    return data
+# end::get_course_data[]
 
-# Iterate through the chunks and create the graph
+# tag::create_chunk[]
+def create_chunk(tx, data):
+    tx.run("""
+        MERGE (c:Course {name: $course})
+        MERGE (c)-[:HAS_MODULE]->(m:Module{name: $module})
+        MERGE (m)-[:HAS_LESSON]->(l:Lesson{name: $lesson, url: $url})
+        MERGE (l)-[:CONTAINS]->(p:Paragraph{text: $text})
+        WITH p
+        CALL db.create.setNodeVectorProperty(p, "embedding", $embedding)
+        """, 
+        data
+        )
+# end::create_chunk[]
 
-# Close the neo4j driver
+# tag::openai[]
+llm = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+# end::openai[]
+
+# tag::neo4j[]
+driver = GraphDatabase.driver(
+    os.getenv('NEO4J_URI'),
+    auth=(
+        os.getenv('NEO4J_USERNAME'),
+        os.getenv('NEO4J_PASSWORD')
+    )
+)
+driver.verify_connectivity()
+# end::neo4j[]
+
+# tag::create[]
+for chunk in chunks:
+    with driver.session(database="neo4j") as session:
+        
+        session.execute_write(
+            create_chunk,
+            get_course_data(llm, chunk)
+        )
+#end::create[]
+
+# tag::close[]
+driver.close()
+# end::close[]
+
+#cypher to see kg
+
+# MATCH (c:Course)-[:HAS_MODULE]->(m:Module)-[:HAS_LESSON]->(l:Lesson)-[:CONTAINS]->(p:Paragraph)
+# RETURN *
+
+#cypher to create vector index
+
+# CREATE VECTOR INDEX paragraphs IF NOT EXISTS
+# FOR (p:Paragraph)
+# ON p.embedding
+# OPTIONS {indexConfig: {
+#  `vector.dimensions`: 1536,
+#  `vector.similarity_function`: 'cosine'
+# }}
+
+#cypher to search similar
+
+# WITH genai.vector.encode(
+#     "How does RAG help ground an LLM?",
+#     "OpenAI",
+#     { token: "sk-..." }) AS userEmbedding
+# CALL db.index.vector.queryNodes('paragraphs', 6, userEmbedding)
+# YIELD node, score
+# MATCH (l:Lesson)-[:CONTAINS]->(node)
+# RETURN l.name, l.url, score
--- llm-vectors-unstructured/create_embeddings.py
+++ llm-vectors-unstructured/create_embeddings.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from openai import OpenAI
-
+from langchain_community.graphs import Neo4jGraph
 llm = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
 response = llm.embeddings.create(
@@ -11,4 +11,26 @@ response = llm.embeddings.create(
         model="text-embedding-ada-002"
     )
 
-print(response.data[0].embedding)
+embedding = response.data[0].embedding
+
+
+#tag::connect[]
+graph = Neo4jGraph(
+    url=os.getenv('NEO4J_URI'),
+    username=os.getenv('NEO4J_USERNAME'),
+    password=os.getenv('NEO4J_PASSWORD')
+)
+#end::connect[]
+
+#tag::query[]
+result = graph.query("""
+CALL db.index.vector.queryNodes('chunkVector', 6, $embedding)
+YIELD node, score
+RETURN node.text, score
+""", {"embedding": embedding})
+#end::query[]
+
+#tag::print[]
+for row in result:
+    print(row['node.text'], row['score'])
+#end::print[]
